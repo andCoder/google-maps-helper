009da34784d8e60295f307f01f9995a6cb227bf3
Admin <andrey1247@mail.ru>
2017-04-10 11:57:19 +1000

    Main plugin file.

---
+++ includes/class-map-render.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 10.04.2017
+ * Time: 11:48
+ */
+
+namespace GoogleMapsHelper\Includes;
+
+/*
+* Class adds JS to the footer section
+*/
+
+class MapRender
+{
+    public function add_script()
+    {
+
+    }
+}
