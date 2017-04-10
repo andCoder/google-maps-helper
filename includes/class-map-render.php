02e79df172a4e716e692a8861d54bfad36a0dfa3
Admin <andrey1247@mail.ru>
2017-04-10 14:05:39 +1000

    Added 'js' script printing.

--- includes/class-map-render.php
+++ includes/class-map-render.php
@@ -12,9 +12,18 @@ namespace GoogleMapsHelper\Includes;
 * Class adds JS to the footer section
 */
 
+if ( defined( 'ABSPATH' ) ) {
+	die( 'Access is denied.' );
+}
+
 class Google_Map_Render
 {
-    private $map_key;
+	const MAP_TYPE_ROADMAP = 'roadmap';
+	const MAP_TYPE_SATELLITE = 'sattelite';
+	const MAP_TYPE_HYBRID = 'hybrid';
+	const MAP_TYPE_TERRAIN = 'terrain';
+
+	private $map_options = array();
 
     function __construct( $key ) {
         $this->set_google_api_key( $key );
@@ -22,8 +31,24 @@ class Google_Map_Render
     }
 
     public function set_google_api_key( $key ) {
-        $map_key = $key;
+	    $this->map_options['api_key'] = $key;
     }
+
+	public function set_map_title( $title ) {
+		$this->map_options['title'] = $title;
+	}
+
+	public function set_marker_icon( $icon ) {
+		$this->map_options['icon'] = $icon;
+	}
+
+	public function set_refresh_interval( $intervalInMillis ) {
+		$this->map_options['refreshInterval'] = $intervalInMillis;
+	}
+
+	public function set_map_type( $type ) {
+		$this->map_options['map_type'] = $type;
+	}
 
     private function add_hooks() {
         add_action( 'wp_print_footer_scripts', array( 'add_script', $this ) );
@@ -31,6 +56,13 @@ class Google_Map_Render
 
     public function add_script()
     {
+	    if ( isset( $this->map_options['api_key'] ) ) {
+		    echo '<script async defer src="https://maps.googleapis.com/maps/api/js?key=' . $this->map_options['api_key']
+		         . '&callback=initMap">
+    </script>';
 
+		    wp_localize_script( MAIN_PATH . '/js/map.js', 'options', $this->map_options );
+		    echo '<script src="js/map.js" type="text/javascript"/>';
+	    }
     }
 }
